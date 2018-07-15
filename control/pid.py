9cb9eb316faa8eb6b0e6982c9928c5e6f584d56f
Kricki <euphiment@gmx.de>
2018-07-15 15:35:53 +0200

    Add high-pass-filter for input of PID

--- control/pid.py
+++ control/pid.py
@@ -25,7 +25,8 @@ class PID:
         self._last_error = 0
         self._sum_error = 0
 
-        self.rate = 0
+        self.fc = 0
+        self._last_pid_input = 0
 
     def update(self, error, dt):
         """Update the PID controller.
@@ -48,12 +49,26 @@ class PID:
             self._sum_error = np.zeros(error.shape)
             self._first = False
 
-        derr = (error-self._last_error)/dt
+        # High-pass filter for input error signal
+        # fc is the cut-off frequency
+        # Implementation according to https://en.wikipedia.org/wiki/High-pass_filter
+        if self.fc > 0:
+            alpha = 1/(1+2*np.pi*self.fc*dt)
+            pid_input = alpha*(self._last_pid_input+error-self._last_error)
+        else:
+            pid_input = error
 
-        self._sum_error += error*dt
         self._last_error = error
 
-        u = (self.kp * error + self.kd * derr + self.ki * self._sum_error)*self.gain
+        #self._sum_error += error*dt
+        self._sum_error += pid_input * dt
+        #derr = (error - self._last_error) / dt
+        derr = (pid_input - self._last_pid_input) / dt
+
+        self._last_pid_input = pid_input
+
+        #u = (self.kp * error + self.kd * derr + self.ki * self._sum_error)*self.gain
+        u = (self.kp * pid_input + self.kd * derr + self.ki * self._sum_error) * self.gain
 
         # clip control value to output limits
         if (self.output_min is not None) or (self.output_max is not None):
